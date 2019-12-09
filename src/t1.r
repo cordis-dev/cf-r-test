76da401036a9429c78a28aa115c11e183fc14fec
cordis-dev <darius@adroiti.com>
2019-12-09 15:21:13 +0200

    Update t1.r

--- src/t1.r
+++ src/t1.r
@@ -1,7 +1,14 @@
-# test
-iris %>%
-  group_by(Species) % > %
-  summarize_all(mean) %>%
-  ungroup %>%
-  gather(measure,value,-Species) %>%
-  arrange(value)
+#' todo: blabla TODO: asdsa
+runCrossValidation <- function(data, pc) {
+  cat(x) ### fixme
+  abv <- 'c:/'
+  x == NA
+  a <- 1;
+  log10(x)
+  lapply(x, log10)
+  return()
+  g_result <- glasso(pc, 1)
+  p_off_d <- sum(g_result$wi != 0 & col(pc) < row(pc))
+  random_pcor <- ggm.simulate.pcor(ncol(data), etaA = 1)
+  stats:::var(c(1, 2, 3))
+}
